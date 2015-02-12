df926f06f5c41e52d2be7c3067fd91fef2a7cc4f
Chris Lunsford <clunsfor@cisco.com>
2015-02-12 15:45:11 -0500

    Adding deploy step to stack, using sudo to execute heighliner
    Change-Id: I106ce058bc786aac9367857fe97e92350e39c925

--- .stack/provision/infra.sh
+++ .stack/provision/infra.sh
@@ -3,3 +3,19 @@
 sudo yum install -y python-heighliner
 
 sudo chown -R vagrant:vagrant /etc/ansible
+
+if [ ! -d /root/.ssh ]; then
+  sudo mkdir /root/.ssh/
+fi
+
+if [ -f /vagrant/.stack/id_rsa ]; then
+  sudo cp /vagrant/.stack/id_rsa /root/.ssh/
+fi
+
+if [ -f /vagrant/.stack/ssh-config ]; then
+  sudo cp /vagrant/.stack/ssh-config /root/.ssh/config
+fi
+
+if [ -f /vagrant/.stack/.ccs.vaultpass.txt ]; then
+  sudo cp /vagrant/.stack/.ccs.vaultpass.txt /etc/ansible/
+fi
