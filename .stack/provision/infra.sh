126e612d54d2385caa839d461273f9c2be9d1f5e
Chris Lunsford <clunsfor@cisco.com>
2015-02-26 14:57:04 -0500

    Cleaning up infra-001 provisioning
    Change-Id: I98a672337090158063a24901d5ff91c230c3bcf5

--- .stack/provision/infra.sh
+++ .stack/provision/infra.sh
@@ -12,10 +12,12 @@ if [ -f /vagrant/.stack/id_rsa ]; then
   sudo cp /vagrant/.stack/id_rsa /root/.ssh/
 fi
 
-if [ -f /vagrant/.stack/ssh-config ]; then
-  sudo cp /vagrant/.stack/ssh-config /root/.ssh/config
+if [ -f /vagrant/.stack/provision/ssh-config ]; then
+  sudo cp /vagrant/.stack/provision/ssh-config /root/.ssh/config
 fi
 
 if [ -f /vagrant/.stack/.ccs.vaultpass.txt ]; then
   sudo cp /vagrant/.stack/.ccs.vaultpass.txt /etc/ansible/
+else
+  echo "changeme" > /etc/ansible/.ccs.vaultpass.txt
 fi
