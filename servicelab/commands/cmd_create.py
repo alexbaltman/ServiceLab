c3e8b3df3ded501e7e4ae0f2ab84b2d2e6a4f90d
aaltman <aaltman@cisco.com>
2015-06-25 11:15:23 -0400

    stubbing out commands
    Change-Id: Ic9973b8001f0fba13626d4ad5ba0459cba16bf73

--- servicelab/commands/cmd_create.py
+++ servicelab/commands/cmd_create.py
@@ -9,11 +9,13 @@ def create(ctx):
     pass
 
 
-@create.command('repo')
+@create.command('repo', short_help='Create a repository in Gerrit')
 @create.argument('name')
-@create.option('-i', '--interactive',)
+@create.option('-i', '--interactive', help='Create repo interactively
+               with extra details')
 # Other option is "source" repository
-@create.option('-t', '--type', default="service")
+@create.option('-t', '--type', default="service", help='Choose a repo
+               type - either service(config management) or project(source)')
 @pass_context
 def repo_new(ctx, name):
     """
@@ -22,3 +24,46 @@ def repo_new(ctx, name):
     directory structure, and creates .nimbus.yml
     """
     click.echo('creating git repository %s ...' % name)
+
+
+@create.command('host')
+@create.argument('name')
+@create.option('-e', '--env', help='Choose an environment to put your host
+               into - use the list command to see what environments are
+               available')
+def host_new(ctx, name):
+    """
+    Creates a host.yaml file in an environment so that a vm can then be
+    booted.
+    """
+    pass
+
+
+@create.command('site')
+@create.argument('name')
+def site_new(ctx, name):
+    """
+    Create a whole site in ccs-data.
+    """
+    pass
+
+
+@create.command('env')
+@create.argument('name')
+def env_new(ctx, name):
+    """
+    Create a new environment in a site in ccs-data.
+    """
+    pass
+
+# RFI: is this the right place for this integration w/ haproxy?
+@create.command('vip')
+# It probably won't take in name
+@create.argument('name')
+# Should be able to create a template for your service and use that too.
+def vip_new(ctx, name):
+    """
+    Create a new VIP in a site in ccs-data in order to integrate your
+    service with haproxy at that particular site.
+    """
+    pass
---
+++ servicelab/commands/cmd_enc.py
@@ -0,0 +1,14 @@
+import click
+from servicelab.stack import pass_context
+
+
+@click.command('enc', short_help='Encrypt a value and it will give you back an
+               encrypted value for you to put into your ccs-data file.')
+@click.argument('value')
+@pass_context
+def enc_value(ctx, value):
+    """
+    Encrypt a value to be put into ccs-data.
+    """
+    ctx.log('Changed files: none')
+    ctx.vlog('debug info')
