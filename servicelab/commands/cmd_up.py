c8bf010832be122118c11efa78e82abef6eacd21
aaltman <aaltman@cisco.com>
2015-06-26 15:24:03 -0400

    Finished stubbing, heading into logic now and iteration on existing parts
    Change-Id: I3be99fb95c7853543beed91bbf96be8fd2645650

--- servicelab/commands/cmd_up.py
+++ servicelab/commands/cmd_up.py
@@ -2,16 +2,24 @@ import click
 from servicelab.stack import pass_context
 
 
-# RFI: What's the best way to implement a non required
-#      argument for <service_name>. --> use group?
-@click.command('up', short_help='')
-@click.option('--ha', help='')
-@click.option('--full', help='comple stack no ha')
-@click.option('', '--osp-aio', help='')
-@click.option('-i', '--interactive', help='')
-@click.option('--username', help='')
-@click.option('--password', help='')
-@click.option
+@click.option('--ha', is_flag=True, help='Enables HA for core OpenStack components \
+              by booting the necessary extra VMs.')
+@click.option('--full', help='Boot complete openstack stack without ha, \
+              unless --ha flag is set.')
+@click.option('--osp-aio', help='Boot a full CCS implementation of the \
+              OpenStack Platform on one VM. Note: This is not the same as \
+              the AIO node deployed in the service cloud.')
+@click.option('-i', '--interactive', help='Walk through booting VMs')
+@click.option('--username', help='Enter the password for the username')
+@click.password_option(help='Enter the gerrit username or \
+                        CEC you want to use.')
+@click.group('up', invoke_without_command=True, short_help="Boots VM(s).")
+@click.pass_context
+def cli(ctx):
+    pass
+
+
+@cli.command(short_help='Boots VM(s).')
 @pass_context
 # RFI: Do we want a explicit prep stage like they do in redhouse-svc
 # RFI: Also we need to think about if we're running latest data
