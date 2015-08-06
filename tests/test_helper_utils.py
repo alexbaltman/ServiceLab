f6ca9927dca75ba3a9c2a62a962305325a95559d
Alex Altman <aaltman@cisco.com>
2015-08-05 23:22:18 -0400

    Fixed helper

--- tests/test_helper_utils.py
+++ tests/test_helper_utils.py
@@ -61,16 +61,14 @@ class TestHelperUtils(unittest.TestCase):
                     TestHelperUtils.YAML_FILE3),
                 "w")
 
-            yaml_dirs = helper_utils.find_all_yaml_recurs(temp_dir)
+            returncode, yaml_dirs = helper_utils.find_all_yaml_recurs(temp_dir)
             self.assertItemsEqual(
                 [yaml_file_1.name, yaml_file_2.name, yaml_file_3.name],
                 yaml_dirs)
-
-        with self.assertRaises(SystemExit) as cm:
-            helper_utils.find_all_yaml_recurs(
+            returncode, yaml_dirs = helper_utils.find_all_yaml_recurs(
                 temp_dir + TestHelperUtils.YAML_EMPTY_DIR)
-            self.assertEqual(cm.exception.code, 1)
-
+            self.assertEqual(returncode, 1)
+            self.assertItemsEqual(yaml_dirs, [])
 
 if __name__ == '__main__':
     unittest.main()
