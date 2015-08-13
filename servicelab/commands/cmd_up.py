ea30b831c246b34d9ee3590df71b8d8a846a6795
aaltman <aaltman@cisco.com>
2015-08-13 17:27:47 -0400

    Combined min and full OS boot workflows. Reorganized options going into up command. Added in some of the front end OS networking workflow. Still very incomplete.
    Change-Id: I79528c23e849dd57cd7b53dd0bf8b4218bf8bf04

--- servicelab/commands/cmd_up.py
+++ servicelab/commands/cmd_up.py
@@ -2,6 +2,7 @@ from servicelab.utils import service_utils
 from servicelab.utils import yaml_utils
 from servicelab.utils import vagrant_utils
 from servicelab.utils import Vagrantfile_utils
+from servicelab.utils import openstack_utils
 from servicelab.utils import helper_utils
 from servicelab.stack import pass_context
 import click
@@ -9,29 +10,43 @@ import sys
 import os
 
 
+@click.option('--full', is_flag=True, default=False, help='Boot complete openstack stack without ha, \
+              unless --ha flag is set. You can not use the min flag with the full flag')
+@click.option('--min', is_flag=True, default=False, help='Boot min openstack stack without ha, \
+              unless --ha flag is set. You can not use min flag with the full flag')
+@click.option('--rhel7', help='Boot a rhel7 vm.')
+@click.option('--target', '-t', help='pick an OSP target vm to boot.')
+@click.option('service_name', default="current")
+@click.option('-r', '--remote', is_flag=True, default=False,
+              help='Boot into an OS environment')
 @click.option('--ha', is_flag=True, default=False, help='Enables HA for core OpenStack components \
               by booting the necessary extra VMs.')
-@click.option('--full', help='Boot complete openstack stack without ha, \
-              unless --ha flag is set.')
+@click.option('-b', '--branch', help='Choose a branch to run against \
+              for ccs-data.')
+@click.option('-u', '--username', help='Enter the password for the username')
+@click.option('-i', '--interactive', help='Walk through booting VMs')
 # @click.option('--osp-aio', help='Boot a full CCS implementation of the \
 #              OpenStack Platform on one VM. Note: This is not the same as \
 #              the AIO node deployed in the service cloud.')
-@click.option('-i', '--interactive', help='Walk through booting VMs')
-@click.option('-r', '--remote', is_flag=True, default=False,
-              help='Boot into an OS environment')
-@click.option('-b', '--branch', help='Choose a branch to run against \
-              for ccs-data.')
-@click.option('--rhel7', help='Boot a rhel7 vm.')
-@click.option('--target', '-t', help='pick an osp target to boot.')
-@click.option('-u', '--username', help='Enter the password for the username')
-# @click.argument('service_name', default="current")
 @click.group('up', invoke_without_command=True, short_help="Boots VM(s).")
 @pass_context
-def cli(ctx, ha, full, osp_aio, interactive, branch, rhel7, username,
+def cli(ctx, ha, full, min, osp_aio, r, interactive, branch, rhel7, username,
         # service_name, target):
         target):
+    if min is True and full is True:
+        ctx.logger.error("You can not use the min flag with the full flag.")
+        sys.exit(1)
+
     if not username:
         returncode, username = helper_utils.set_user(ctx.path)
+        if returncode > 0:
+            import getpass
+            ctx.logger.debug("Couldn't set user from .git/config will try\
+                              to use current running user.")
+            username = getpass.getuser()
+            if not username:
+                ctx.logger.debug("Still couldn't set username. Exiting.")
+                sys.exit(1)
 
     returncode, current_service = helper_utils.get_current_service(ctx.path)
     # RFI: may not matter if returns 1 if we aren't using current_service
@@ -47,22 +62,12 @@ def cli(ctx, ha, full, osp_aio, interactive, branch, rhel7, username,
         click.echo("vagrant up %s" % (target))
         a = vagrant_utils.Connect_to_vagrant(vmname=target,
                                              path=ctx.path)
+        # TODO: up the provided vm --> check vagrant_utils
         a.v.up()
         service_utils.run_this('vagrant hostmanager')
+        sys.exit(0)
         # ======================================================
 
-    # writeout vagrant.yaml w/ current vms
-    # read the profiles to see what you need to boot
-        # or a prep for a service --> where to store profiles
-        # the whole one is defined it's the small one.
-    # if not in profile
-    # #check if host exists
-    # # if not
-    # ## read host from templ
-    # ## write host
-    # ## if it's not in
-
-    # ALL ===============================================
     if not os.path.isfile(os.path.join(ctx.path, "vagrant.yaml")):
         with open(os.path.join(ctx.path, "vagrant.yaml"), 'w') as f:
             # Note: we could pass here or just close it, either way
@@ -73,13 +78,15 @@ def cli(ctx, ha, full, osp_aio, interactive, branch, rhel7, username,
     path_to_utils = os.path.join(path_to_utils[0], "utils")
     pathto_vagrantyaml_templ = os.path.join(path_to_utils, "vagrant.yaml")
 
-    # MIN WORKFLOW ======================================
-    returncode, allmin_vms = yaml_utils.getmin_OS_vms(pathto_vagrantyaml_templ)
+    if min:
+        returncode, allmy_vms = yaml_utils.getmin_OS_vms(pathto_vagrantyaml_templ)
+    elif full:
+        returncode, allmy_vms = yaml_utils.getfull_OS_vms(pathto_vagrantyaml_templ)
     if returncode > 0:
-        ctx.logger.error("Couldn't get the vms for the min OS env")
+        ctx.logger.error("Couldn't get the vms from the vagrant.yaml.")
         sys.exit(1)
     try:
-        for k in allmin_vms:
+        for k in allmy_vms:
             if ha:
                 ha_vm = k.replace("001", "002")
                 returncode, ha_vm_dict = yaml_utils.gethost_byname(ha_vm,
@@ -88,63 +95,49 @@ def cli(ctx, ha, full, osp_aio, interactive, branch, rhel7, username,
                     ctx.logger.error("Couldn't get the vm {0} for HA".format(ha_vm))
                     sys.exit(1)
                 else:
-                    allmin_vms.append(ha_vm_dict)
+                    allmy_vms.append(ha_vm_dict)
             yaml_utils.host_add_vagrantyaml(ctx.path,
                                             "vagrant.yaml",
                                             k,
                                             "ccs-dev-1",
-                                            memory=allmin_vms[k]['memory'],
-                                            box=allmin_vms[k]['box'],
-                                            role=allmin_vms[k]['role'],
-                                            profile=allmin_vms[k]['profile'],
-                                            domain=allmin_vms[k]['domain'],
-                                            storage=allmin_vms[k]['storage'],
-                                            )
-
-    except IOError as e:
-        ctx.logger.error("{0} for vagrant.yaml in {1}".format(e, ctx.path))
-        sys.exit(1)
-
-        # Write out Vagrantfile
-        Vagrantfile_utils.overwrite_vagrantfile(ctx.path)
-
-        # Boot VMs
-
-        # Provision VMs separatly if possible.
-
-    # FULL WORKFLOW ================================
-    returncode, all_OS_vms = yaml_utils.getfull_OS_vms(pathto_vagrantyaml_templ, "001")
-    if returncode > 0:
-        ctx.logger.error("Couldn't get the vms for the full OS env")
-    try:
-        for k in all_OS_vms:
-            if ha:
-                ha_vm = k.replace("001", "002")
-                returncode, ha_vm_dict = yaml_utils.gethost_byname(ha_vm,
-                                                                   pathto_vagrantyaml_templ)
-                if returncode > 0:
-                    ctx.logger.error("Couldn't get the vm {0} for HA".format(ha_vm))
-                    sys.exit(1)
-                else:
-                    all_OS_vms.append(ha_vm_dict)
-            yaml_utils.host_add_vagrantyaml(ctx.path,
-                                            "vagrant.yaml",
-                                            k,
-                                            "ccs-dev-1",
-                                            memory=all_OS_vms[k]['memory'],
-                                            box=all_OS_vms[k]['box'],
-                                            role=all_OS_vms[k]['role'],
-                                            profile=all_OS_vms[k]['profile'],
-                                            domain=all_OS_vms[k]['domain'],
-                                            storage=all_OS_vms[k]['storage'],
+                                            memory=allmy_vms[k]['memory'],
+                                            box=allmy_vms[k]['box'],
+                                            role=allmy_vms[k]['role'],
+                                            profile=allmy_vms[k]['profile'],
+                                            domain=allmy_vms[k]['domain'],
+                                            storage=allmy_vms[k]['storage'],
                                             )
     except IOError as e:
         ctx.logger.error("{0} for vagrant.yaml in {1}".format(e, ctx.path))
         sys.exit(1)
 
-        # Write out Vagrantfile
+        if r:
+            # TODO: Write out remote provider Vagrantfile
+            # TODO: set env vars.
+            Vagrantfile_utils.overwrite_vagrantfile(ctx.path)
+            # TODO: test returncodes
+            a = openstack_utils.SLab_OS(path=ctx.path, password='XXXX', username=username,
+                                        base_url='XXXX')
+            a.login_or_gettoken()
+            a.login_or_gettoken(tenant_id='xxxx')
+            a.connect_to_neutron()
+            returncode, router = a.create_router()
+            router_id = router['id']
+            returncode, network = a.create_network()
+            returncode, subnet = a.create_subnet()
+            a.add_int_to_router(router_id, subnet['id'])
 
-        # Boot VMs
+            name = a.create_name_for("network", append="mgmt")
+            returncode, mgmt_network = a.create_network(name=name)
+            name = a.create_name_for("subnet", append="mgmt")
+            returncode, mgmt_subnet = a.create_subnet(name=name,
+                                                      cidr='192.168.1.0/24')
+            a.add_int_to_router(router_id, mgmt_subnet['id'])
+        else:
+            # Write out local provider Vagrantfile
+            Vagrantfile_utils.overwrite_vagrantfile(ctx.path)
+
+        # Boot VMs --> vagrant up in the .stack folder should get all vms
 
         # Provision VMs separatly if possible.
 
