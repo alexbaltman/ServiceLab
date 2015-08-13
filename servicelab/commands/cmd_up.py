7376b78fd92213f3aecb47c6b93b71ba6b8ca57d
aaltman <aaltman@cisco.com>
2015-08-13 14:23:09 -0400

    Added workflow for full OS vms.
    Change-Id: I0dbd81e86a689f56a46bd478281ede0b2bedb177

--- servicelab/commands/cmd_up.py
+++ servicelab/commands/cmd_up.py
@@ -1,6 +1,7 @@
 from servicelab.utils import service_utils
 from servicelab.utils import yaml_utils
 from servicelab.utils import vagrant_utils
+from servicelab.utils import Vagrantfile_utils
 from servicelab.utils import helper_utils
 from servicelab.stack import pass_context
 import click
@@ -38,23 +39,17 @@ def cli(ctx, ha, full, osp_aio, interactive, branch, rhel7, username,
         ctx.error("Failed to get the current service")
         sys.exit(1)
 
-    # ======================================================
     # #Dev testing Block for aaltman
     # attrs = vars(ctx)
     # print ', '.join("%s: %s" % item for item in attrs.items())
 
-    # CL/JP stack method
     if target:
         click.echo("vagrant up %s" % (target))
         a = vagrant_utils.Connect_to_vagrant(vmname=target,
                                              path=ctx.path)
         a.v.up()
         service_utils.run_this('vagrant hostmanager')
-    # service_utils.run_this('vagrant ssh infra-001 -c cp "/etc/ansible"; \
-    # cd "/opt/ccs/services/%s; sudo heighliner \
-    # --dev --debug deploy"' % (os.path.join(ctx.path, "hosts"),
-    # service_name))
-    # ======================================================
+        # ======================================================
 
     # writeout vagrant.yaml w/ current vms
     # read the profiles to see what you need to boot
@@ -70,19 +65,20 @@ def cli(ctx, ha, full, osp_aio, interactive, branch, rhel7, username,
     # ALL ===============================================
     if not os.path.isfile(os.path.join(ctx.path, "vagrant.yaml")):
         with open(os.path.join(ctx.path, "vagrant.yaml"), 'w') as f:
+            # Note: we could pass here or just close it, either way
+            #       we'll get an empty file
             f.write("")
 
-    path_to_utils = os.path.split(path)
+    path_to_utils = os.path.split(ctx.path)
     path_to_utils = os.path.join(path_to_utils[0], "utils")
     pathto_vagrantyaml_templ = os.path.join(path_to_utils, "vagrant.yaml")
 
     # MIN WORKFLOW ======================================
     returncode, allmin_vms = yaml_utils.getmin_OS_vms(pathto_vagrantyaml_templ)
-    if returcode > 0:
+    if returncode > 0:
         ctx.logger.error("Couldn't get the vms for the min OS env")
         sys.exit(1)
     try:
-        ha_vms = []
         for k in allmin_vms:
             if ha:
                 ha_vm = k.replace("001", "002")
@@ -97,26 +93,68 @@ def cli(ctx, ha, full, osp_aio, interactive, branch, rhel7, username,
                                             "vagrant.yaml",
                                             k,
                                             "ccs-dev-1",
-                                            memory=allmin_vms[k]['memory']
-                                            box=allmin_vms[k]['box']
-                                            role=allmin_vms[k]['role']
-                                            profile=allmin_vms[k]['profile']
-                                            domain=allmin_vms[k]['domain']
-                                            storage=allmin_vms[k]['storage']
+                                            memory=allmin_vms[k]['memory'],
+                                            box=allmin_vms[k]['box'],
+                                            role=allmin_vms[k]['role'],
+                                            profile=allmin_vms[k]['profile'],
+                                            domain=allmin_vms[k]['domain'],
+                                            storage=allmin_vms[k]['storage'],
                                             )
 
     except IOError as e:
         ctx.logger.error("{0} for vagrant.yaml in {1}".format(e, ctx.path))
         sys.exit(1)
 
+        # Write out Vagrantfile
+        Vagrantfile_utils.overwrite_vagrantfile(ctx.path)
+
+        # Boot VMs
+
+        # Provision VMs separatly if possible.
+
     # FULL WORKFLOW ================================
     returncode, all_OS_vms = yaml_utils.getfull_OS_vms(pathto_vagrantyaml_templ, "001")
-    if returcode > 0:
+    if returncode > 0:
         ctx.logger.error("Couldn't get the vms for the full OS env")
+    try:
+        for k in all_OS_vms:
+            if ha:
+                ha_vm = k.replace("001", "002")
+                returncode, ha_vm_dict = yaml_utils.gethost_byname(ha_vm,
+                                                                   pathto_vagrantyaml_templ)
+                if returncode > 0:
+                    ctx.logger.error("Couldn't get the vm {0} for HA".format(ha_vm))
+                    sys.exit(1)
+                else:
+                    all_OS_vms.append(ha_vm_dict)
+            yaml_utils.host_add_vagrantyaml(ctx.path,
+                                            "vagrant.yaml",
+                                            k,
+                                            "ccs-dev-1",
+                                            memory=all_OS_vms[k]['memory'],
+                                            box=all_OS_vms[k]['box'],
+                                            role=all_OS_vms[k]['role'],
+                                            profile=all_OS_vms[k]['profile'],
+                                            domain=all_OS_vms[k]['domain'],
+                                            storage=all_OS_vms[k]['storage'],
+                                            )
+    except IOError as e:
+        ctx.logger.error("{0} for vagrant.yaml in {1}".format(e, ctx.path))
+        sys.exit(1)
 
-    # Added FULL HA =============================
-    returncode, all_OS_havms = yaml_utils.getfull_OS_vms(pathto_vagrantyaml_templ, "002")
-    if returcode > 0:
-        ctx.logger.error("Couldn't get the vms for the full OS env")
+        # Write out Vagrantfile
+
+        # Boot VMs
+
+        # Provision VMs separatly if possible.
 
     # RHEL7 WORKFLOW ===============================
+    # add new rhel7 vm to ccs-dev-1
+    # add to vagrant.yaml
+    # boot it alone
+
+    # SERVICE VM WORKFLOW ==========================
+    # service_utils.run_this('vagrant ssh infra-001 -c cp "/etc/ansible"; \
+    # cd "/opt/ccs/services/%s; sudo heighliner \
+    # --dev --debug deploy"' % (os.path.join(ctx.path, "hosts"),
+    # service_name))
