d9e325c83f694d6717994ab908fc5e8a0b66077b
Raghu Katti <ragkatti@cisco.com>
2015-08-28 18:51:46 -0400

    Added sudo level Added unit tests for gocd Added gocd setup in jenkins
    Change-Id: I1ab3eac41c3640ed3ab0237a531b46647f2f1864
    
    Added sudo level
    
    Change-Id: I5beff4172cf6bbb9b276eb804defd645a641a51b
    
    Added go cd setup
    
    Change-Id: Id2d287c31ac5364f0ddc6980d038b91ed099d873
    
    Fixed wget url
    
    Change-Id: Ieb9302a81593e8be82b387d67c837b2b04ad97e2
    
    Added fixes to
    
    Change-Id: I108b0e67ea0edeba83622d34f8cd702878a5358b
    
    Added checks
    
    Change-Id: I4489bec053af795c11a3de4f97fecd789dce65da
    
    Added htpasswd setting
    
    Change-Id: I7ec78c4d5a9dbe5555620ed8a0a4c4dfe234ea8c
    
    Debug
    
    Change-Id: I847cf7962bac9e854cccb96f75d14530b72136c0
    
    Debug
    
    Change-Id: I488eb3dd97774f5d0bd53866679d16b763b16f13
    
    Added fixes
    
    Change-Id: I8fb6ac88608d62d7f935ae034b8aec7e2c94adf3

---
+++ tests/test_gocd_utils.py
@@ -0,0 +1,145 @@
+import os
+import unittest
+import getpass
+from tests.helpers import temporary_dir
+from servicelab.utils import gocd_utils
+from servicelab.commands import cmd_pipe
+from servicelab.commands import cmd_list
+from servicelab.commands import cmd_find
+import yaml
+import shutil
+import time
+import requests
+from requests.auth import HTTPBasicAuth
+from BeautifulSoup import BeautifulSoup
+from servicelab.stack import Context
+from click.testing import CliRunner
+
+
+class TestGoCdUtils(unittest.TestCase):
+
+    GOCD_SERVER = "localhost:8153"
+    GOCD_USER = "raju"
+    GOCD_PASS = "badger"
+    PIPELINE_NAME = "servicelab-test-only-pipeline"
+    T_SCHED = "{\"locked\":false,\"paused\":false,\"schedulable\":true}"
+    F_SCHED = "{\"locked\":false,\"paused\":false,\"schedulable\":false}"
+    ENDING_LOG = "-------------------End of job log for pipeline"
+
+    """
+    TestGoCdUtils class is a unittest class for yaml_utils.
+    Attributes:
+        ctx:  Context object of servicelab module.
+    """
+
+    def setUp(self):
+        """ Setup variables required to test the os_provider functions
+        """
+        self.ctx = Context()
+        url = "http://{0}/go/tab/admin/pipelines/{1}.json".format(
+            TestGoCdUtils.GOCD_SERVER, TestGoCdUtils.PIPELINE_NAME)
+        payload = {
+            'url': 'https://github.com/Silverpop/sample-helloworld-ant.git',
+            'scm': 'git',
+            'builder': 'ant',
+            'buildfile': 'build.xml',
+            'target': 'main'}
+        res = requests.post(
+            url,
+            auth=HTTPBasicAuth(
+                TestGoCdUtils.GOCD_USER,
+                TestGoCdUtils.GOCD_PASS),
+            data=payload)
+
+    def test_cmd_pipeline_status(self):
+        """ Tests run command.
+        """
+        runner = CliRunner()
+        result = runner.invoke(cmd_pipe.cli,
+                               ['status',
+                                TestGoCdUtils.PIPELINE_NAME,
+                                '-g',
+                                TestGoCdUtils.GOCD_USER,
+                                '-h',
+                                TestGoCdUtils.GOCD_PASS,
+                                '-s',
+                                TestGoCdUtils.GOCD_SERVER])
+        self.assertItemsEqual(
+            result.output.strip(),
+            TestGoCdUtils.T_SCHED)
+
+    def test_cmd_run(self):
+        """ Tests run command.
+        """
+        runner = CliRunner()
+        result = runner.invoke(cmd_pipe.cli,
+                               ['run',
+                                TestGoCdUtils.PIPELINE_NAME,
+                                '-g',
+                                TestGoCdUtils.GOCD_USER,
+                                '-h',
+                                TestGoCdUtils.GOCD_PASS,
+                                '-s',
+                                TestGoCdUtils.GOCD_SERVER])
+        result = runner.invoke(cmd_pipe.cli,
+                               ['status',
+                                TestGoCdUtils.PIPELINE_NAME,
+                                '-g',
+                                TestGoCdUtils.GOCD_USER,
+                                '-h',
+                                TestGoCdUtils.GOCD_PASS,
+                                '-s',
+                                TestGoCdUtils.GOCD_SERVER])
+        self.assertItemsEqual(
+            result.output.strip(),
+            TestGoCdUtils.F_SCHED)
+
+    def test_cmd_list(self):
+        """ Tests run command.
+        """
+        runner = CliRunner()
+        result = runner.invoke(cmd_list.cli,
+                               ['pipes',
+                                '-g',
+                                TestGoCdUtils.GOCD_USER,
+                                '-h',
+                                TestGoCdUtils.GOCD_PASS,
+                                '-s',
+                                TestGoCdUtils.GOCD_SERVER])
+        self.assertTrue(TestGoCdUtils.PIPELINE_NAME in result.output.strip())
+
+    def test_cmd_find(self):
+        """ Tests run command.
+        """
+        runner = CliRunner()
+        result = runner.invoke(cmd_find.cli,
+                               ['pipe',
+                                TestGoCdUtils.PIPELINE_NAME,
+                                '-g',
+                                TestGoCdUtils.GOCD_USER,
+                                '-h',
+                                TestGoCdUtils.GOCD_PASS,
+                                '-s',
+                                TestGoCdUtils.GOCD_SERVER])
+        self.assertItemsEqual(
+            result.output.strip(),
+            TestGoCdUtils.PIPELINE_NAME)
+
+    def test_cmd_log(self):
+        """ Tests run command.
+        """
+        runner = CliRunner()
+        result = runner.invoke(cmd_pipe.cli,
+                               ['log',
+                                TestGoCdUtils.PIPELINE_NAME,
+                                '-g',
+                                TestGoCdUtils.GOCD_USER,
+                                '-h',
+                                TestGoCdUtils.GOCD_PASS,
+                                '-s',
+                                TestGoCdUtils.GOCD_SERVER])
+        self.assertTrue(TestGoCdUtils.ENDING_LOG in result.output.strip())
+
+
+if __name__ == '__main__':
+    unittest.main()
