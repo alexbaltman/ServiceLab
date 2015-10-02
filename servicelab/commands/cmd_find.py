7ad80202fb6152275362d872d66b31fb8c40a7d4
kunanda <kunanda@cisco.com>
2015-10-02 17:15:14 -0400

    new changes add the callback to validate the IP address
    Change-Id: Ia8ea7022049450ec93d1d9fbdad21655c8c70d9a
    
    Fixed a bug
    
    Change-Id: Ic5c803df9896974a19ce494cb4f4c2becb68608c

--- servicelab/commands/cmd_find.py
+++ servicelab/commands/cmd_find.py
@@ -18,7 +18,6 @@ from requests.auth import HTTPBasicAuth
 
 from servicelab.stack import pass_context
 from servicelab.utils import jenkins_utils
-from servicelab.utils import context_utils
 from servicelab.utils import gerrit_functions
 from servicelab.utils import helper_utils
 
@@ -47,7 +46,16 @@ def find_repo(ctx, search_term):
     for elem in repo_list:
         match_obj = re.search(search_term, elem, re.I)
         if match_obj:
-            print elem
+            click.echo(elem)
+
+
+def validate_artifact_ip_cb(ctx, param, value):
+    """
+    If ip is none then provide the default ip for artifactory.
+    """
+    if not value:
+        value = ctx.obj.get_artifactory_info()
+    return value
 
 
 @cli.command('artifact', short_help='Find an artifact in artifactory')
@@ -64,7 +72,8 @@ def find_repo(ctx, search_term):
               '--ip_address',
               help='Provide the artifactory url ip address and port '
                    'no in format http://<ip:portno>.',
-              default=None)
+              default=None,
+              callback=validate_artifact_ip_cb)
 @pass_context
 def find_artifact(ctx, search_term, ip_address, user, password):
     """
@@ -78,6 +87,15 @@ def find_artifact(ctx, search_term, ip_address, user, password):
     res = requests.get(find_url, auth=HTTPBasicAuth(user, password))
     for val in json.loads(res.content)["results"]:
         click.echo(val["uri"])
+
+
+def validate_pipe_ip_cb(ctx, param, value):
+    """
+    If ip is none then provide the default ip for gocd.
+    """
+    if not value:
+        value = ctx.obj.get_gocd_info()
+    return value
 
 
 @cli.command('pipe', short_help='Find a Go deploy pipeline')
@@ -98,10 +116,10 @@ def find_artifact(ctx, search_term, ip_address, user, password):
               required=True)
 @click.option('-ip',
               '--ip_address',
-              default=context_utils.get_gocd_ip(),
+              default=None,
               help='Provide the go server ip address and port number '
                    'in format <ip_address:portnumber>.',
-              required=True)
+              callback=validate_pipe_ip_cb)
 @pass_context
 def find_pipe(ctx, search_term, localrepo, user, password, ip_address):
     """
@@ -118,16 +136,10 @@ def find_pipe(ctx, search_term, localrepo, user, password, ip_address):
         pipelines = soup.findAll('pipeline')
         return pipelines
 
-    if ip_address is None:
-        ip_address = ctx.get_gocd_info()
-
-    servicesdirs = []
-    if os.path.isdir(os.path.join(ctx.path, "services")):
-        servicesdirs = os.listdir(os.path.join(ctx.path, "services"))
-
-    pipelines = _get_pipeline()
-
-    for pipeline in pipelines:
+    def _get_match(pipeline):
+        """
+        find the match in the pipeline for services and other projects
+        """
         search_string = pipeline['href']
         split_string = search_string.split('/')
         search_string = search_term
@@ -140,9 +152,30 @@ def find_pipe(ctx, search_term, localrepo, user, password, ip_address):
                     if sdir.startswith("service-"):
                         service = sdir.split("service-", 1)[1]
                         if service == match_obj.group():
-                            click.echo(split_string[len(split_string) - 2])
+                            return split_string[len(split_string) - 2]
             else:
-                click.echo(split_string[len(split_string) - 2])
+                return split_string[len(split_string) - 2]
+        return None
+
+    servicesdirs = []
+    if os.path.isdir(os.path.join(ctx.path, "services")):
+        servicesdirs = os.listdir(os.path.join(ctx.path, "services"))
+
+    pipelines = _get_pipeline()
+
+    for pipeline in pipelines:
+        match_str = _get_match(pipeline)
+        if match_str:
+            click.echo(match_str)
+
+
+def validate_build_ip_cb(ctx, param, value):
+    """
+    If ip is none then provide the default ip for jenkins.
+    """
+    if not value:
+        value = ctx.obj.get_jenkins_info()
+    return value
 
 
 @cli.command('build', short_help='Find a build')
@@ -159,16 +192,13 @@ def find_pipe(ctx, search_term, localrepo, user, password, ip_address):
               '--ip_address',
               help='Provide the jenkinsserv url ip address and port'
                    'no in format <ip:portno>.',
-              default=context_utils.get_jenkins_url()
-              )
+              default=None,
+              callback=validate_build_ip_cb)
 @pass_context
 def find_build(_, search_term, user, password, ip_address):
     """
     Searches through the build search term.
     """
-    if ip_address is None:
-        ip_address = ctx.get_jenkins_info()
-
     server = jenkins_utils.get_server_instance(ip_address, user, password)
     for key in server.keys():
         match_obj = re.search(search_term, key, re.M | re.I)
