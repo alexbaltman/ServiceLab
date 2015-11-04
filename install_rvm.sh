ad0687f7a12248d555cb0e2740e9eba4c3533998
Alex Altman <aaltman@cisco.com>
2015-11-04 18:11:21 -0500

    Removing install_rvm b/c should not need w/ the install script from Raghu and removing stack-Vagrantfile b/c we do not need anymore.

--- install_rvm.sh
+++
@@ -1,6 +0,0 @@
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -L get.rvm.io | bash -s stable
-#Add user(s) to rvm group
-#Make sure rvm is in path in .bashrc/.zshrc
-#rvm install ruby-2.0.0-p481
-#rvm 2.0.0-p481@servicelab --create --ruby-version
