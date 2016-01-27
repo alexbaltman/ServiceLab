39e57becea5a8da505ee255bc5c1814c1956ea04
Raghu Katti <ragkatti@cisco.com>
2016-01-27 08:05:08 -0500

    Fixed ruby at 1.9.3
    Change-Id: I34a68f05f08d96575f89f347052b87f1cc228637

--- slab_setup.sh
+++ slab_setup.sh
@@ -8,7 +8,8 @@ gpg2 --keyserver hkp://keys.gnupg.net:80 --recv-keys 409B6B1796C275462A170311380
 sudo yum -y install python-devel
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 sudo python get-pip.py
-sudo yum -y install gcc  ruby-devel rubygems
+sudo yum -y install gcc
+sudo yum -y install clang
 fi
 if [[ "$OSTYPE" == "darwin"* ]]
 then
@@ -21,5 +22,9 @@ sudo pip install -r requirements.txt
 sudo pip install -e .
 curl -L get.rvm.io | bash -s stable
 curl -sSL https://get.rvm.io | bash -s stable --ruby
+source ~/.profile
+rvm install 1.9.3 --with-gcc=clang
+sudo yum -y install rubygems
 gem install bundler
 bundle install
+ruby -v
