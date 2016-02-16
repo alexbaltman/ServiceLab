cdd78b7eeb3b112bc7a63a32875878e2025dbd21
Paul Ketner <paketner@cisco.com>
2016-02-16 18:44:07 -0500

    Tweaked stack.py after rebase with develop
    Change-Id: I812e2162e79cfd6f7e2cd549da51fb8dad4d3830

--- servicelab/stack.py
+++ servicelab/stack.py
@@ -2,11 +2,10 @@
 stack
 """
 import os
+import re
 import sys
 import click
 import logging
-
-#from servicelab.utils import helper_utils
 
 # Global Variables
 # auto envvar prefix will take in any env vars that are prefixed with STK
@@ -27,14 +26,6 @@ class Context(object):
         pkey_name (str)    - servicelab public key
 
         branch (str)
-        verbose (boolean)
-        vverbose (boolean)
-        debug (boolean)
-        logger (logger)
-
-        file_handler       - Create filehandler that logs everything.
-        console_handler    - Create console handler that logs up to error msg.
-        formatter          - Create formatter and add it to the handlers
 
         __gerrit_test_info - Gerrit staging server
         __gerrit_info      - Gerrit server
@@ -42,14 +33,13 @@ class Context(object):
 
     """
     def __init__(self):
+        self.branch = "master"
         self.path = os.path.join(os.path.dirname(__file__), '.stack')
         self.config = os.path.join(os.path.dirname(__file__),
                                    '.stack/stack.conf')
 
         self.pkey_name = "servicelab/utils/public_key.pkcs7.pem"
 
-        # Setup Logging
-        self.branch = "master"
         self.__gerrit_test_info = {"hostname": "ccs-gerrit-stg.cisco.com",
                                    "port": 29418}
         self.__gerrit_info = {"hostname": "ccs-gerrit.cisco.com",
@@ -62,15 +52,11 @@ class Context(object):
             {"url": "https://ccs-jenkins.cisco.com"}
         self.__pulp_info = \
             {"url": "https://ccs-mirror.cisco.com"}
-        self.username = 'paketner'
-        self.password = None
-
-        # set the user name according to this defined hierarchy
-        returncode, self.username = helper_utils.get_gitusername(self.path)
+        returncode, self.username = self.get_gitusername(self.path)
         if returncode > 0:
-            helper_utils.get_loginusername()
-
+            self.username = getpass.getuser()
         self.password = None
+
         if os.getenv("OS_USERNAME"):
             self.username = os.getenv("OS_USERNAME")
             self.password = os.getenv("OS_PASSWORD")
@@ -78,6 +64,10 @@ class Context(object):
         if os.getenv("STK_USERNAME"):
             self.username = os.getenv("STK_USERNAME")
             self.password = os.getenv("STK_PASSWORD")
+
+        if not self.username:
+            self.logger.error('Unable to determine username')
+            sys.exit(1)
 
     def get_gerrit_server(self):
         """
@@ -149,6 +139,29 @@ class Context(object):
         if interactive and not self.password:
             self.password = click.prompt("password", hide_input=True, type=str)
         return self.password
+
+    def get_gitusername(self, path):
+        """
+        Extract username from the original git clone of the servicelab repo
+        """
+        matches = None                                                        
+        username = ""                                                         
+                                                                              
+        path_to_reporoot = os.path.split(path)                                
+        path_to_reporoot = os.path.split(path_to_reporoot[0])                 
+        path_to_reporoot = path_to_reporoot[0]                                
+                                                                              
+        regex = re.compile(r'://([a-z]*)@?(ccs|cis)-gerrit.cisco.com')        
+        with open(os.path.join(path_to_reporoot, ".git", "config"), 'r') as f:
+            for line in f.readlines():                                        
+                matches = re.search(regex, line)                              
+                if matches:                                                   
+                    username = matches.group(1)                               
+                    if not username:                                          
+                        return 1, username                                    
+                    return 0, username                                        
+        return 1, username                                                    
+
 
 pass_context = click.make_pass_decorator(Context, ensure=True)
 
